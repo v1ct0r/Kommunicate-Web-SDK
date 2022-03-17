5d3125c1134e7759cdabacf904b7236d77f384f5
yanakozlova <jasenkozlova@gmail.com>
2022-03-17 18:56:20 +0200

    [fix] try to fix gap between msgs

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -11631,7 +11631,9 @@ var userOverride = {
                     message = messageContainer.querySelector(
                         'div[data-msgkey="' + MCK_BOT_MESSAGE_QUEUE[0] + '"]'
                     );
-                    if (!currentMessageObject.metadata.needTypingIndicator) $applozic('.km-typing-wrapper').remove();
+                    const typingElem = $applozic('.km-typing-wrapper');
+                    $applozic('.km-typing-wrapper').remove();
+                    if (currentMessageObject.metadata.needTypingIndicator) $applozic('.mck-group-inner').append(typingElem);
                     if (message) {
                         message.classList.remove('n-vis');
 
