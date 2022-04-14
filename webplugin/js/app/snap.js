62b92334553deb01afc4361aa6adf1a6baa8da26
yanakozlova <jasenkozlova@gmail.com>
2022-04-14 10:55:34 +0300

    [fix] - reload send button

--- webplugin/js/app/snap.js
+++ webplugin/js/app/snap.js
@@ -773,6 +773,7 @@ $applozic.extend(true, Snap, {
             textBox.attr('data-label', '');
             textBox.attr('contenteditable', false);
             Snap.reloadElement('mck-textbox-container', 'mck-text-box');
+            Snap.reloadElement('mck-textbox-container', 'send-button-wrapper');
         } else {
             var metadata = msg.metadata;
             var hintTextForTextInput = metadata.hasOwnProperty('text_input_hint') ? metadata.text_input_hint : '';
@@ -780,6 +781,7 @@ $applozic.extend(true, Snap, {
             textBox.attr('data-label', hintTextForTextInput);
             textBox.attr('contenteditable', metadata.enable_text_input);
             Snap.reloadElement('mck-textbox-container', 'mck-text-box');
+            Snap.reloadElement('mck-textbox-container', 'send-button-wrapper');
         }
 
         let checkEnable = (typeof msg.metadata.enable_text_input === 'boolean' && msg.metadata.enable_text_input) || msg.metadata.enable_text_input == 'true';
