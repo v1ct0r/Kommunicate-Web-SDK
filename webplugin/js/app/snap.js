b13ff2294a097b9166a9b0392581db26ca5b8916
yanakozlova <jasenkozlova@gmail.com>
2022-05-17 14:05:17 +0300

    [ref] - refactor reloading elements in changeTextInputState func

--- webplugin/js/app/snap.js
+++ webplugin/js/app/snap.js
@@ -782,9 +782,6 @@ $applozic.extend(true, Snap, {
                 textBox.attr('aria-label', '')
                 extBox.attr('aria-labelledby', '');
                 textBox.attr('contenteditable', false);
-                Snap.reloadElement('mck-textbox-container', 'mck-text-box');
-                Snap.reloadElement('mck-textbox-container', 'send-button-wrapper');
-                Snap.reloadElement('mck-sidebox-content', 'quick-reply-container');
             } else {
                 var metadata = msg.metadata;
                 var hintTextForTextInput = metadata.hasOwnProperty('text_input_hint') ? metadata.text_input_hint : '';
@@ -793,15 +790,14 @@ $applozic.extend(true, Snap, {
                 textBox.attr('aria-labelledby', isEnable ? hintTextForTextInput + "Multiline text box, double tap to edit" : hintTextForTextInput);
                 textBox.attr('aria-label', isEnable ? hintTextForTextInput + "Multiline text box, double tap to edit" : hintTextForTextInput);
                 textBox.attr('contenteditable', metadata.enable_text_input);
-                Snap.reloadElement('mck-textbox-container', 'mck-text-box');
-                Snap.reloadElement('mck-textbox-container', 'send-button-wrapper');
-                Snap.reloadElement('mck-sidebox-content', 'quick-reply-container');
             }
 
             if (msg.hasOwnProperty('metadata') && msg.metadata.is_numeric_input) {
                 textBox.attr('pattern', '\d*');
                 textBox.attr('inputmode', 'numeric');
             }
+            Snap.reloadElement('mck-textbox-container', 'mck-text-box');
+            Snap.reloadElement('mck-textbox-container', 'send-button-wrapper');
         }, 300);
     },
     sessionTimeout: function () {
