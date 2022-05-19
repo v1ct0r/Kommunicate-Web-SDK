bc7b2a703dd862b0e8f7bca9f23342807fc1e845
yanakozlova <jasenkozlova@gmail.com>
2022-05-19 15:38:05 +0300

    [fix] dont show empty message

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -11661,7 +11661,6 @@ var userOverride = {
                 var currentMessageObject = ALStorage.getMessageByKey(
                   MCK_BOT_MESSAGE_QUEUE[0]
                 );
-                currentMessageObject.message && mckMessageLayout.messageClubbing(false);
                 if (!document.querySelector('.km-typing-wrapper') && currentMessageObject.message) {
                     $mck_msg_inner.append(
                         '<div class="km-typing-wrapper"><div class="km-typing-indicator"></div><div class="km-typing-indicator"></div><div class="km-typing-indicator"></div></div>'
@@ -11680,6 +11679,7 @@ var userOverride = {
                         'div[data-msgkey="' + MCK_BOT_MESSAGE_QUEUE[0] + '"]'
                     );
                     $applozic('.km-typing-wrapper').remove();
+                    currentMessageObject.message && mckMessageLayout.messageClubbing(false);
 
                     MCK_BOT_MESSAGE_QUEUE.shift();
 
@@ -11689,7 +11689,7 @@ var userOverride = {
                         Snap.changeTextInputState(currentMessageObject);
                     }
                     if (message) {
-                        message.classList.remove('n-vis');
+                        currentMessageObject.message && message.classList.remove('n-vis');
                         // if ($quick_reply_container.children().length > 0 && MCK_BOT_MESSAGE_QUEUE.length < 1
                         //   && !currentMessageObject.metadata.is_close_conversation)  {
                         //     Snap.changeVisibilityStateForElement(
