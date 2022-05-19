9533828aa29258e2ea30753d28e42b41c83bd8f8
yanakozlova <jasenkozlova@gmail.com>
2022-05-19 12:14:40 +0300

    [fix] - $quick_reply_container.append timeout

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8574,7 +8574,7 @@ var userOverride = {
                               0
                             );
                             _this.initDatepicker();
-                        }, MCK_BOT_MESSAGE_DELAY + 1500)
+                        }, MCK_BOT_MESSAGE_DELAY + 2100)
 
                     }
                 } else {
