b8e0907f6390d1f96256ac0c411750b826868b1c
yanakozlova <jasenkozlova@gmail.com>
2022-08-02 11:25:46 +0300

    [fix] increase timeout for message

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8573,7 +8573,7 @@ var userOverride = {
                               0
                             );
                             _this.initDatepicker();
-                        }, MCK_BOT_MESSAGE_DELAY * MCK_BOT_MESSAGE_QUEUE.length + 1300)
+                        }, MCK_BOT_MESSAGE_DELAY * MCK_BOT_MESSAGE_QUEUE.length + 1500)
 
                     }
                 } else {
