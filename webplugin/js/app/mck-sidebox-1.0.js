dd9c2a47f1c349db76570e5acc80f2f99aba703a
yanakozlova <jasenkozlova@gmail.com>
2022-08-17 19:28:52 +0300

    Revert "[test] test without timeout between"
    This reverts commit 0a3f4863ff5aa61c355a9dcffa93e19300ab687d.

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8605,7 +8605,7 @@ var userOverride = {
                               0
                             );
                             _this.initDatepicker();
-                        }, MCK_BOT_MESSAGE_DELAY * MCK_BOT_MESSAGE_QUEUE.length)
+                        }, MCK_BOT_MESSAGE_DELAY * MCK_BOT_MESSAGE_QUEUE.length + 1500)
 
                     }
                 }
