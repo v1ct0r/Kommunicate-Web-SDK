1d906fa7b9997a9218bc1331c31410c8f7ab854e
Anton Khoda <anton.khoda@chisw.com>
2022-08-18 16:07:14 +0300

    loging ALStorage

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -5449,7 +5449,7 @@ var userOverride = {
 
                 const browserInfo = detect.parse(navigator.userAgent);
                 let localPayload = message.metadata.payload;
-                w.console.log('ALStorage -> ', ALStorage.getLatestMessageArray());
+                w.console.log('ALStorage -> ', ALStorage.getLatestMessageArray().find(e => e.key === message.key));
                 w.console.log('message -> ', message);
                 w.console.log('messagePxy -> ', messagePxy);
                 let currentButtonInfo = {};
