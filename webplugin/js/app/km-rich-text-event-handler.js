a742610c84ba636afb13a2f530076422fc8a89a1
Anton Khoda <anton.khoda@chisw.com>
2022-09-06 10:30:58 +0300

    added reconnect after regular button click

--- webplugin/js/app/km-rich-text-event-handler.js
+++ webplugin/js/app/km-rich-text-event-handler.js
@@ -803,6 +803,7 @@ Snap.richMsgEventHandler = {
                 Snap.hideMessage(e.target);
             }
         }
+        window.Applozic.ALSocket.reconnect();
     },
     processClickOnListItem: function (e) {
         var target = e.currentTarget;
