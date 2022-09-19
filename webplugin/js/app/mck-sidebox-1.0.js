db92d3edda6bcd4a06c02dd4da576cf2ee5815e2
Anton Khoda <anton.khoda@chisw.com>
2022-09-19 10:56:19 +0300

    HG-220 added timeout of changeTextInputState run check in addMessage fun

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8988,15 +8988,22 @@ var userOverride = {
                             });
                         }
                         $textMessage.append(x);
-                        setTimeout(function () {
-                            if (arrayOfAllMessages) {
+                        if(arrayOfAllMessages){
+                            setTimeout(function () {
                                 if (isLastSavedMessageInDialog) {
                                     Snap.changeTextInputState(msg);
                                 }
-                            } else {
-                                Snap.changeTextInputState(msg);
-                            }
-                        }, MCK_BOT_MESSAGE_DELAY + 1000)
+                            }, MCK_BOT_MESSAGE_DELAY + 1000)
+                        }
+                        // setTimeout(function () {
+                        //     if (arrayOfAllMessages) {
+                        //         if (isLastSavedMessageInDialog) {
+                        //             Snap.changeTextInputState(msg);
+                        //         }
+                        //     } else {
+                        //         Snap.changeTextInputState(msg);
+                        //     }
+                        // }, MCK_BOT_MESSAGE_DELAY + 1000)
                     }
                 } else {
                     $textMessage.html(emoji_template);
