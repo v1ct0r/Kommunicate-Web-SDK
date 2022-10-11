9bb1314de672a6b7c5e2c7bf984681d7b243d028
yanakozlova <jasenkozlova@gmail.com>
2022-10-11 13:50:49 +0300

    [fix] add km-chat-faq-list to $quick_reply_container

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -8603,6 +8603,7 @@ var userOverride = {
                     (kmRichTextMarkup.includes('km-quick-replies') && !kmRichTextMarkup.includes('km-div-slider'))
                     || kmRichTextMarkup.includes('km-btn-hidden-form')
                     || kmRichTextMarkup.includes('km-cta-multi-button-links-container')
+                    || kmRichTextMarkup.includes('km-chat-faq-list')
                 ) {
                     //don't need to append buttons to the messageTemplate arrea,
                     //because we append them to the quick-reply-container
