2e5af4bf89bdc1bdd0ebf5a62fd512745f50440f
Anton Khoda <antonhoda98@gmail.com>
2022-10-20 10:09:46 +0300

    HG-97 fixed messagePxy.buttonId check for card buttons

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -5466,7 +5466,7 @@ var userOverride = {
                         localButtonId = localPayloadArray.find(e => e.title === messagePxy.message).button_id;
                     }
                     if(!localButtonId && localPayloadArray && Array.isArray(localPayloadArray)){
-                        localPayloadArray = localPayload = message.metadata.payload;
+                        localPayloadArray = message.metadata.payload;
                         let localButtonIdArr = localPayloadArray.filter(e => e.hasOwnProperty('buttons')).map(e => e = e.buttons).flat();
                         if(localButtonIdArr.length){
                             localButtonId = localButtonIdArr.find(e => e.name === messagePxy.message).button_id;
