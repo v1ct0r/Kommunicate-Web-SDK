708ac04bfc93acbd372f619c636d145c147c90fd
Anton Khoda <antonhoda98@gmail.com>
2022-11-08 10:13:05 +0200

    fixed hide_weekend attr

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -9118,10 +9118,10 @@ var userOverride = {
                         minuteIncrement: 60,
                         "disable": [
                             function(date) {
-                                // if(popupDate.dataset.hide_weekend === 'true'){
-                                //     return (date.getDay() === 0 || date.getDay() === 6);
-                                // }
-                                return true
+                                if(popupDate.dataset.hideweekend === 'true'){
+                                    return (date.getDay() === 0 || date.getDay() === 6);
+                                }
+                                return false
                             }
                         ],
                     });
