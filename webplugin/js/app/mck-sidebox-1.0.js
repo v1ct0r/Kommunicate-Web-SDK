79a4df5002bfc030bf7c03ef9ce7c25ed826ee05
Sasha Chekh <oleksandr.chekh@gera-it.com>
2023-03-24 12:07:09 +0200

    fix: make quick reply appear time shorter

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -11950,7 +11950,7 @@ var userOverride = {
                                         0
                                     );
                                     _this.initDatepicker();
-                                }, MCK_BOT_MESSAGE_DELAY + 1500)
+                                }, MCK_BOT_MESSAGE_DELAY )
 
                         }
                     }
