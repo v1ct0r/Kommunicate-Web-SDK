d4ff28cb55caedbb4d555822b75bb6dc1ff6e4f3
Sasha Chekh <oleksandr.chekh@gera-it.com>
2023-04-28 14:05:57 +0300

    feat: make quick reply appear time shorter

--- webplugin/js/app/mck-sidebox-1.0.js
+++ webplugin/js/app/mck-sidebox-1.0.js
@@ -11991,7 +11991,7 @@ var userOverride = {
                                         0
                                     );
                                     _this.initDatepicker();
-                                }, MCK_BOT_MESSAGE_DELAY )
+                                }, 500 )
 
                         }
                     }
